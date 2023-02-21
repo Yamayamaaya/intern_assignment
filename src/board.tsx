81b94e665a69d6efc6ef2adf7ac5161ab5fef673
Yamayamaaya <yugo139013@gmail.com>
2023-02-21 17:16:31 +0900

    12th-commit

--- src/board.tsx
+++ src/board.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { FC } from "react";
 import Square from "./square";
 
 //board
@@ -7,38 +8,30 @@ interface boardProps {
   squares: Array<oneSquareType>;
   onClick: (i: number) => void;
 }
-
-class Board extends React.Component<boardProps, {}> {
-  renderSquare = (i: number) => {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
+const Board: FC<boardProps> = (props) => {
+  const renderSquare = (i: number) => {
+    return <Square value={props.squares[i]} onClick={() => props.onClick(i)} />;
   };
 
-  render = () => {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
-  };
-}
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
 
 export default Board;
