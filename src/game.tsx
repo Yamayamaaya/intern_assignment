59bfd523294a02e847d5b8db12a6e3ba1e0f98b5
Yamayamaaya <yugo139013@gmail.com>
2023-02-22 10:33:46 +0900

    13th-commit

--- src/game.tsx
+++ src/game.tsx
@@ -1,58 +1,85 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import Board from "./board";
 
-type oneSquareType = "O" | "X" | null;
+type State = {
+  history: [{ squares: oneSquareType[] }];
+  stepNumber: number;
+  xIsNext: boolean;
+};
+
+type ForReducer = {
+  mode: "handleClick" | "jumpTo";
+  i: number;
+};
+
+const initialState: State = {
+  history: [{ squares: Array<oneSquareType>(9).fill(null) }],
+  stepNumber: 0,
+  xIsNext: true,
+};
+
+const reducer = (state: State, f: ForReducer): State => {
+  switch (f.mode) {
+    case "handleClick":
+      const historySlice = state.history.slice(
+        0,
+        state.stepNumber + 1
+      ) as State["history"];
+      const current = historySlice[historySlice.length - 1];
+      const squares = current.squares.slice();
+      if (calculateWinner(squares) || squares[f.i]) {
+        return state;
+      }
+      squares[f.i] = state.xIsNext ? "X" : "O";
+      console.log(historySlice.concat([{ squares: squares }]));
+      return {
+        history: historySlice.concat([
+          { squares: squares },
+        ]) as State["history"],
+        stepNumber: historySlice.length,
+        xIsNext: !state.xIsNext,
+      };
+    case "jumpTo":
+      return {
+        history: state.history,
+        stepNumber: f.i,
+        xIsNext: f.i % 2 === 0,
+      };
+    default:
+      return state;
+  }
+};
 
 const Game = () => {
-  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
-  const [stepNumber, setStepNumber] = useState(0);
-  const [xIsNext, setXIsNext] = useState(true);
-  const handleClick = (i: number) => {
-    const historySlice = history.slice(0, stepNumber + 1);
-    const current = historySlice[historySlice.length - 1];
-    const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = xIsNext ? "X" : "O";
-    setHistory(
-      historySlice.concat([
-        {
-          squares: squares,
-        },
-      ])
-    );
-    setStepNumber(historySlice.length);
-    setXIsNext(!xIsNext);
-  };
-
-  const jumpTo = (step: number) => {
-    setStepNumber(step);
-    setXIsNext(step % 2 === 0);
-  };
-
-  const current = history[stepNumber];
+  const [state, dispatch_ox] = useReducer(reducer, initialState);
+  const current = state.history[state.stepNumber];
   const winner = calculateWinner(current.squares);
 
-  const moves = history.map((step, move) => {
+  const moves = state.history.map((step, move) => {
     const desc = move ? "Go to move #" + move : "Go to game start";
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{desc}</button>
+        <button onClick={() => dispatch_ox({ mode: "handleClick", i: move })}>
+          {desc}
+        </button>
       </li>
     );
   });
+
   let status;
   if (winner) {
     status = "Winner: " + winner;
   } else {
-    status = "Next player: " + (xIsNext ? "X" : "O");
+    status = "Next player: " + (state.xIsNext ? "X" : "O");
   }
 
   return (
     <div className="game">
       <div className="game-board">
-        <Board squares={current.squares} onClick={(i) => handleClick(i)} />
+        <Board
+          squares={current.squares}
+          onClick={(i) => dispatch_ox({ mode: "jumpTo", i: i })}
+        />
       </div>
       <div className="game-info">
         <div>{status}</div>
@@ -61,6 +88,8 @@ const Game = () => {
     </div>
   );
 };
+
+type oneSquareType = "O" | "X" | null;
 
 const calculateWinner = (squares: Array<oneSquareType>) => {
   const lines = [
